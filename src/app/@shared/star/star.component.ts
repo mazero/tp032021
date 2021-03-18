c6fe9ed04c3652f20c4eba9dde9720d81d511351
Kamy <camille@mazero.ch>
2021-03-18 12:34:53 +0100

    star component

--- src/app/@shared/star/star.component.ts
+++ src/app/@shared/star/star.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 @Component({
   selector: 'app-star',
   templateUrl: './star.component.html',
   styleUrls: ['./star.component.scss']
 })
 export class StarComponent implements OnInit {
+  public values: number[] = [1,2,3,4,5];
+
+  @Input() rating: number = 3;
+  @Output() ratingChange: EventEmitter<number> = new EventEmitter<number>()
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  public onClick(value: number) {
+    console.log('The clicked value is', value);
+    this.ratingChange.emit(value);
   }
 
 }
